c8991d7f042596ebabbc2c06aaab4034a51f9baa
Rene Windegger <rene@windegger.wtf>
2018-04-09 12:23:39 +0200

    Added a logger that writes to std::cout.

--- Scylla/Logger.h
+++ Scylla/Logger.h
@@ -1,57 +1,67 @@
 #pragma once
 
 #include <windows.h>
+#include <iostream>
 
 // Abstract class for logging text.
 class Logger
 {
 public:
 
-	virtual void log(const WCHAR * format, ...);
-	virtual void log(const CHAR * format, ...);
+    virtual void log(const WCHAR * format, ...);
+    virtual void log(const CHAR * format, ...);
 
 protected:
 
-	virtual void write(const WCHAR * str) = 0;
-	virtual void write(const CHAR * str);
+    virtual void write(const WCHAR * str) = 0;
+    virtual void write(const CHAR * str);
+};
+
+class ConsoleLogger : public Logger
+{
+public:
+    ConsoleLogger() {};
+    void setWindow(HWND window) {};
+protected:
+    void write(const WCHAR* str) override { std::wcout << L"[SCYLLA] " << str << L"\n"; };
 };
 
 // Dummy logger which does absolutely nothing
 class DummyLogger : public Logger
 {
 public:
-	DummyLogger() {};
+    DummyLogger() {};
 private:
-	void write(const WCHAR * str) {};
+    void write(const WCHAR * str) {};
 };
 
 class FileLog : public Logger
 {
 public:
 
-	FileLog(const WCHAR * fileName);
+    FileLog(const WCHAR * fileName);
 
 private:
 
-	void write(const WCHAR * str);
-	void write(const CHAR * str);
+    void write(const WCHAR * str);
+    void write(const CHAR * str);
 
-	WCHAR filePath[MAX_PATH];
+    WCHAR filePath[MAX_PATH];
 };
 
 class ListboxLog : public Logger
 {
 public:
 
-	ListboxLog() : window(0) { }
-	ListboxLog(HWND window);
+    ListboxLog() : window(0) { }
+    ListboxLog(HWND window);
 
-	void setWindow(HWND window);
+    void setWindow(HWND window);
 
 private:
 
-	void write(const WCHAR * str);
-	//void write(const CHAR * str);
+    void write(const WCHAR * str);
+    //void write(const CHAR * str);
 
-	HWND window;
+    HWND window;
 };
